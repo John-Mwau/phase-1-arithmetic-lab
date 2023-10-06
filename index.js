b1143e8fe44a97dad07690327ee5aff7f703bfaa
john <jmulimwau@gmail.com>
2023-10-06 18:10:29 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+    let num1=31;
+    let num2=2;
+    const multiply=num1*num2;
+   
+const random = Math.floor(Math.random() * 100) + 1;
+let num3=36;
+let num4=8;
+let mod=num3%num4;
+
+const numberSet = [17, 9, 20, 15, 5];
+const max = Math.max(...numberSet);
+
+
+    
